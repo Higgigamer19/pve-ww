7773bab315f520fc3c727f653521f407b0b0db8b
dhelgerson <tah568@hpc.msstate.edu>
2025-07-14 14:37:38 -0500

    me did whoopsies

--- push-container.sh
+++ push-container.sh
@@ -2,6 +2,6 @@
 echo 'pushing changes to warewulf. (this may take some time)...'
 docker build -t pve-ib .
 docker save {,-o}pve-ib
-wwctl import pve-ib --force
+wwctl image import pve-ib --force
 wwctl image build pve-ib
 wwctl overlay build
