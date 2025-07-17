8fe6d9669211a84b1aa1b7968434218e96b2b9af
root <root@warewulf.z3ph7r.com>
2025-07-17 14:53:47 -0500

    Modifying scripts to support  argument

--- new-nodes.sh
+++ new-nodes.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 #Defaults:
 BASE_IMAGE=base
+STATED_LOCATION=/mnt/pve-node-states/
 
 #Help Menu:
 show_help() {
     cat << EOF
-Usage: ${0##*/} [OPTIONS]
+Usage: ${0##*/} [OPTIONS] {node names}
 
 Options:
-  -h, --help                 Show this help message and exit
-  -b, --baseImage IMAGE      Specify the base image (default=base)
+  -h, --help                 	Show this help message and exit
+  -b, --baseImage IMAGE      	Specify the base image (default=base)
+  -l, --statedLocation PATH	Specify the location of the stated nfs (default=/mnt/pve-node-states/)
+
+Examples:
+  ${0##*/} -b base -l /mnt/pve-node-states/ z-01
+  ${0##*/} --baseImage gpu-base --statedLocation /mnt/pve-node-states/ z-01-gpu
+  ${0##*/} --baseImage=deb-base --statedLocation=/mnt/pve-node-states/ z-01-deb
 EOF
 }
 
@@ -26,7 +33,7 @@ for arg in "$@"; do
                 echo -e "\nUsing base image $2\n"
 		shift 2
             else
-                echo "Error: --baseImage requires a value."
+                echo "ERROR: --baseImage requires a value."
                 exit 1
             fi
             ;;
@@ -35,6 +42,21 @@ for arg in "$@"; do
 	    echo -e "\nUsing base image ${1#*=}\n"
             shift
             ;;
+	-l|--statedLocation)
+	    if [[ -n "$2" && ! "$2" =~ ^- ]]; then
+		STATED_LOCATION="$2"
+		echo -e "\nUsing stated location $2\n"
+		shift 2
+	    else
+		echo "ERROR: --statedLocation requires a value."
+		exit 1
+	    fi
+	    ;;
+	--statedLocation=*)
+	    STATED_LOCATION="${1#*=}"
+	    echo -e "\n Using stated location $2\n"
+	    shift
+	    ;;
     esac
 done
 
@@ -46,7 +68,7 @@ for node in "$@"; do
     echo -e "\nCreating node $node"
     wwctl node add $node
     echo -e "\nCopying base fs to $node's fs"
-    rsync -a /mnt/pve-node-states/{$BASE_IMAGE,$node}/
+    rsync -a $STATED_LOCATION/{$BASE_IMAGE,$node}/
     echo -e "\nCompleted $node's fs" 
   fi
 done
--- update-overlays.sh
+++ update-overlays.sh
@@ -1,10 +1,54 @@
 #!/bin/bash
+#Defaults:
+STATED_LOCATION=/mnt/pve-node-states/
+
+#Help Menu:
+show_help() {
+    cat << EOF
+Usage: ${0##*/} [OPTIONS] {node names}
+
+Options:
+  -h, --help                 	Show this help message and exit
+  -l, --statedLocation PATH	Specify the location of the stated nfs (default=/mnt/pve-node-states/)
+
+Examples:
+  ${0##*/} -l /mnt/pve-node-states/
+  ${0##*/} --statedLocation /mnt/pve-node-states/
+  ${0##*/} --statedLocation=/mnt/pve-node-states/
+EOF
+}
+
+#
+for arg in "$@"; do
+    case "$arg" in
+        -h|--help)
+            show_help
+            exit 0
+            ;;
+	-l|--statedLocation)
+	    if [[ -n "$2" && ! "$2" =~ ^- ]]; then
+		STATED_LOCATION="$2"
+		echo -e "\nUsing stated location $2\n"
+		shift 2
+	    else
+		echo "ERROR: --statedLocation requires a value."
+		exit 1
+	    fi
+	    ;;
+	--statedLocation=*)
+	    STATED_LOCATION="${1#*=}"
+	    echo -e "\n Using stated location $2\n"
+	    shift
+	    ;;
+    esac
+done
+
 
 echo "Building all overlays"
 wwctl overlay build
 
 for node in $(echo "$(wwctl node list -j)" | jq -r 'keys[]'); do
     echo "Updating overlays for $node"
-    cat /opt/warewulf/var/warewulf/provision/overlays/$node/__SYSTEM__.img | cpio -idD /mnt/pve-node-states/$node/
-    cat /opt/warewulf/var/warewulf/provision/overlays/$node/__RUNTIME__.img | cpio -idD /mnt/pve-node-states/$node/
+    cat /opt/warewulf/var/warewulf/provision/overlays/$node/__SYSTEM__.img | cpio -idD $STATED_LOCATION/$node/
+    cat /opt/warewulf/var/warewulf/provision/overlays/$node/__RUNTIME__.img | cpio -idD $STATED_LOCATION/$node/
 done
